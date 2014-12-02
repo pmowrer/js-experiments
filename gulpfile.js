2513547852164b864a69188e9b1395818d5c151b
Patrick Mowrer <pmowrer@gmail.com>
2014-12-01 23:43:15 -0500

    Let gulp compile LESS

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 var gulp = require('gulp'),
+    path = require('path'),
     browserSync = require('browser-sync'),
-    reload = browserSync.reload;
+    reload = browserSync.reload,
+    less = require('gulp-less');
+
+gulp.task('less', function() {
+    gulp.src('./assets/css/index.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./assets/css/'));
+});
 
 // Based on recipe:
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/server-with-livereload-and-css-injection.md
-gulp.task('serve', function() {
+gulp.task('serve', ['compile'], function() {
     browserSync({
         server: {
             baseDir: './'
         }
     });
 
+    gulp.watch('./assets/css/**/*.less', ['compile']);
     gulp.watch(['index.html', 'assets/css/**/*.css', 'app/**/*.js'], {}, reload);
 });
+
+gulp.task('compile', ['less']);
